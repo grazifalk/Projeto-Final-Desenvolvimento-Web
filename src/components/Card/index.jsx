de511fd5d59baaf818006714aa82487717d48d24
Grazi Falk <83370784+grazifalk@users.noreply.github.com>
2022-11-05 03:45:16 -0300

    Atualização App

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,17 +1,31 @@
-import {Card } from "react-bootstrap";
-import {Link} from "react-router-dom";
+import { Card } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import Api from "../../service/Api.js";
+import { Link } from "react-router-dom";
 
-export const Card = (props) => {
+export const CardProdutos = (props) => {
+  const [produtos, setProduto] = useState([]);
 
-    return (
-        <Card className="my-3 rounded">
-        <Card.Img src={props.fotoLink} height={250} width={250} />
-        <Card.Body>
-          <Card.Title>{props.nome}</Card.Title>
-          <Card.Text>{props.valor}</Card.Text>
-          {/* <Link to={`/produto/${props.id}`}>Detalhe</Link> */}
-        </Card.Body>
-      </Card>
-    );
+  useEffect(() => {
+    Api.get("/produto")
+      .then((response) => setProduto(response.data))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
 
-}
+  return (
+    <div>
+      {produtos.map((props) => { return (
+        <Card className="my-3 rounded" key={props.id}>
+          <Card.Img src={props.fotoLink} height={250} width={250} />
+          <Card.Body>
+            <Card.Title>{props.nome}</Card.Title>
+            <Card.Text>{props.valor}</Card.Text>
+            {/* <Link to={`/produto/${props.id}`}>Detalhe</Link> */}
+          </Card.Body>
+        </Card>)
+      })}
+    </div>
+  );
+};
--- src/screens/Produtos/index.jsx
+++ src/screens/Produtos/index.jsx
@@ -1,19 +1,5 @@
-import Api from "../../service/Api.js";
-import React, { useEffect, useState } from "react";
+import { CardProdutos } from "../../components/Card/index.jsx";
 
 export const Produtos = () => {
-
-    const [produtos, setProduto] = useState([]);
-
-    useEffect(() => {
-        Api
-          .get("/produto")
-          .then((response) => setProduto(response.data))
-          .catch((err) => {
-            console.error("ops! ocorreu um erro" + err);
-          });
-      }, []);
-      
-      return (<>{produtos.map((produto) => {return (<><h1 key={produto.id}>{produto.fotoLink}</h1> <img src={produto.fotoLink} alt="" /> </>) })}</>)
-
-}
+  return <CardProdutos />
+};
